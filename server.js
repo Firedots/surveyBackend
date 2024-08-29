dacefc487b8e912598dd1c3db3d1f8c800a06b40
Firedots <robinschaber@web.de>
2024-08-29 10:57:15 +0200

    qq
    qq

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+
+// Route to handle POST requests
+app.post('/receive-word', (req, res) => {
+  const { word } = req.body;
+  console.log(`Received word: ${word}`);
+  res.send('Word received');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
